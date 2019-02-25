c2544de1a239c9dca5366726c1e34f59e1c3453e
Aleem Pasha <aleem.pasha@jdplc.com>
2019-02-25 19:07:27 +0530

    Download excel code added

--- src/components/TextReader.vue
+++ src/components/TextReader.vue
@@ -4,14 +4,13 @@
       <h1>Upload Files</h1>
       <div class="dropbox">
         <input class="input-file" type="file" @change="loadTextFromFile" multiple="multiple" accept=".text">
-        <p v-if="isInitial">Drag your file(s) here to begin<br> or click to browse</p>
-        <p v-if="isSaving">Uploading {{ fileCount }} files...</p>
+        <p>Drag your file here to begin<br> or <br>click to browse</p>
       </div>
     </div>
     <div class="output" v-if="showTable">
       <div class="header">
         <p class="main_heading">Final Output In Table Format</p>
-        <!-- <button class="button right">Download Excel</button> -->
+        <button class="button right" @click="downloadExcel()">Download Excel</button>
       </div>
       <Tabs
         :tabs="tabs"
@@ -21,7 +20,7 @@
       <div class="content">
         <div v-if="currentTab === 'tab1'">
           <vue-good-table
-            :columns="columns"
+            :columns="columns_filtered"
             :rows="matchedRecords"
             max-height="500px"
             :fixed-header="true"
@@ -36,7 +35,7 @@
         </div>
         <div v-if="currentTab === 'tab2'">
           <vue-good-table
-            :columns="columns"
+            :columns="columns_without_filter"
             :rows="allRecords"
             max-height="500px"
             :fixed-header="true"
@@ -71,8 +70,6 @@ export default {
     fileCount: '',
     allRecords: [],
     showTable: false,
-    isSaving: false,
-    isInitial: true,
     replaceStrings: [
       "The SKU data provided is different from what's already in the Amazon catalog. The standard_product_id data provided matches ASIN",
       "but the following data is different from what's already in the Amazon catalog:",
@@ -84,7 +81,31 @@ export default {
       "The SKU data provided is different from what's already in the Amazon catalog. The item_id data provided matches ASIN",
       ". If this is the right ASIN for your product, update your data to match what's in the Amazon catalog. If it's not the right ASIN, make sure that your data for item_id is correct.",
     ],
-    columns: [
+    columns_filtered: [
+      {
+        label: 'Product',
+        field: 'product',
+        type: 'number',
+      },
+      {
+        label: 'SKU',
+        field: 'SKU',
+        type: 'number',
+      },
+      {
+        label: 'Error Code',
+        field: 'error-code',
+      },
+      {
+        label: 'ASIN',
+        field: 'ASIN',
+      },
+      {
+        label: 'Error Message',
+        field: 'error-msg',
+      },
+    ],
+    columns_without_filter: [
       {
         label: 'Product',
         field: 'product',
@@ -119,11 +140,9 @@ export default {
       if (!files.length)
         return;
       const finalOutput = [];
-      this.isInitial = false;
       const afterFilter = [];
       const numFiles = files.length;
       this.fileCount = numFiles;
-      this.isSaving = true;
       for (let i = 0; i < numFiles; i++) {
         const file = files[i];
         const reader = new FileReader();
@@ -136,26 +155,29 @@ export default {
               const singleRow = line.split('\t'); // Splitting the single line into coloumns
               if (singleRow['1'] !== 'undefined') {
                 const productSKU = singleRow['1'].split('.');
-                if (singleRow['2'] === '8541' || singleRow['2'] === '8542') {
-                  let replaceString = '';
-                  let that = this;
-                  replaceString = that.replaceAllStrings(this.replaceStrings['0'], '', singleRow['4']);
-                  this.replaceStrings.forEach(function (str) {
-                    replaceString = that.replaceAllStrings(str, '', replaceString);
-                  });
-                  let obj = {};
-                  obj['product'] = productSKU[0];
-                  obj['SKU'] = productSKU[1];
-                  obj['error-code'] = singleRow['2'];
-                  obj['error-msg'] = replaceString;
-                  finalOutput.push(obj);
-                } else {
-                  let obj = {};
-                  obj['product'] = productSKU[0];
-                  obj['SKU'] = productSKU[1];
-                  obj['error-code'] = singleRow['2'];
-                  obj['error-msg'] = singleRow['4'];
-                  afterFilter.push(obj);
+                if(productSKU[1]) {
+                  if (singleRow['2'] === '8541' || singleRow['2'] === '8542') {
+                    let replaceString = '';
+                    let that = this;
+                    replaceString = that.replaceAllStrings(this.replaceStrings['0'], '', singleRow['4']);
+                    this.replaceStrings.forEach(function (str) {
+                      replaceString = that.replaceAllStrings(str, '', replaceString);
+                    });
+                    let obj = {};
+                    obj['product'] = productSKU[0];
+                    obj['ASIN'] = replaceString.trim().substring(0,10);
+                    obj['SKU'] = productSKU[1];
+                    obj['error-code'] = singleRow['2'];
+                    obj['error-msg'] = replaceString.trim().substring(11);
+                    finalOutput.push(obj);
+                  } else if(productSKU[1] !== 'undefined' || productSKU[1] !== undefined) {
+                    let obj = {};
+                    obj['product'] = productSKU[0];
+                    obj['SKU'] = productSKU[1];
+                    obj['error-code'] = singleRow['2'];
+                    obj['error-msg'] = singleRow['4'];
+                    afterFilter.push(obj);
+                  }
                 }
               }
             }
@@ -176,6 +198,73 @@ export default {
         str = str.replace(find, replace);
       }
       return str;
+    },
+    downloadExcel() {
+      if(this.currentTab === 'tab1') {
+        this.JSONToCSVConvertor(this.matchedRecords, "Filtered Data", true);
+      } else if(this.currentTab === 'tab2') {
+        this.JSONToCSVConvertor(this.allRecords, "Data after Filter", true);
+      }
+    },
+    JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+      //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+      const arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+      let CSV = '';
+      //Set Report title in first row or line
+      CSV += ReportTitle + '\r\n\n';
+      //This condition will generate the Label/Header
+      if (ShowLabel) {
+        let row = "";
+        //This loop will extract the label from 1st index of on array
+        for (const index in arrData[0]) {
+          //Now convert each value to string and comma-seprated
+          row += index + ',';
+        }
+        row = row.slice(0, -1);
+        //append Label row with line break
+        CSV += row + '\r\n';
+      }
+
+      //1st loop is to extract each row
+      for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+        //2nd loop will extract each column and convert it in string comma-seprated
+        for (var index in arrData[i]) {
+          row += '"' + arrData[i][index] + '",';
+        }
+        row.slice(0, row.length - 1);
+        //add a line break after each row
+        CSV += row + '\r\n';
+      }
+      if (CSV == '') {
+          alert("Invalid data");
+          return;
+      }
+      //Generate a file name
+      var fileName = "MyReport_";
+      //this will remove the blank-spaces from the title and replace it with an underscore
+      fileName += ReportTitle.replace(/ /g,"_");
+
+      //Initialize file format you want csv or xls
+      var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+      // Now the little tricky part.
+      // you can use either>> window.open(uri);
+      // but this will not work in some browsers
+      // or you will not get the correct file extension
+
+      //this trick will generate a temp <a /> tag
+      var link = document.createElement("a");
+      link.href = uri;
+
+      //set the visibility hidden so it will not effect on your web-layout
+      link.style = "visibility:hidden";
+      link.download = fileName + ".xls";
+
+      //this part will append the anchor tag and remove it after automatic click
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     }
   }
 };
